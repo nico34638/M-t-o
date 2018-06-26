93e11e88c21b7d6eee49a01995ae41c0c3e01e98
nico34638 <nico24093@gmail.com>
2018-06-26 22:59:49 +0200

    add rain

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const weatherIcons = {
   "Rain": "wi wi-day-rain",
+  "Thunderstorm": "wi wi-day-rain",
   "Clouds": "wi wi-day-cloudy",
   "Clear": "wi wi-day-sunny",
   "Snow": "wi wi-day-snow",
-  "mist": "wi wi-day-fog",
+  "Fog": "wi wi-day-fog",
   "Drizzle": "wi wi-day-sleet"
 }
 function capitalize(str){
